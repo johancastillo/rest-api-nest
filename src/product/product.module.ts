f4aa463ad9573257092dc3bacae0abfc2a964da5
Johan Castillo <jcjohan2707@gmail.com>
2022-02-11 16:20:51 -0400

    Product Service

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
+import {productSchema} from './schemas/product.schema'
+import {MongooseModule} from '@nestjs/mongoose'
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {name: 'Product', schema: productSchema}
+    ])
+  ],
   controllers: [ProductController],
   providers: [ProductService]
 })
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import {Model} from 'mongoose';
+import {InjectModel} from '@nestjs/mongoose';
+import {Product} from './interfaces/product.interface';
+import {CreateProductDTO} from './dto/product.dto';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+
+    constructor(@InjectModel('Product') private productModel:Model<Product>) {
+
+    }
+
+    // Get all products
+    async getProducts(product): Promise<Product[]>{
+        let products = await this.productModel.find()
+        return products;
+    }
+
+    // Get single product
+    async getProduct(productId:string): Promise<Product>{
+        let product = await this.productModel.findById(productId);
+        return product;
+    }
+
+    // Create product
+    async createProduct(createProductDTO:CreateProductDTO){
+        const product = await new this.productModel(createProductDTO);
+        const newProduct = await product.save();
+        return newProduct;
+    }
+
+    async deleteProduct(productId:string):Promise<Product>{
+        const deleteProduct = await this.productModel.findByIdAndDelete(productId);
+        return deleteProduct;
+    }
+
+    async updateProduct(productId:string, createProductDTO:CreateProductDTO):Promise<Product>{
+        const product = await this.productModel.findByIdAndUpdate(productId, createProductDTO, {new: true});
+        return product;
+    }
+
+}
