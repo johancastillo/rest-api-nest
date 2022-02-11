e5da4e02f7311d07781eaef307c575dce766cab5
Johan Castillo <jcjohan2707@gmail.com>
2022-02-11 10:45:48 -0400

    Create Schema for Product

---
+++ src/product/schemas/product.schema.ts
@@ -0,0 +1,14 @@
+import {Schema} from 'mongoose';
+
+
+export const productSchema = new Schema({
+    name: {type: String, required: true},
+    description: String,
+    imageURL: String,
+    price: Number,
+    createAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
